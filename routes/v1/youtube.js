48db92c1ee4d405211d46cfabbaf964aaf97cb7b
Jonathan <jon.chua51@gmail.com>
2023-03-25 20:27:57 -0700

    v2 youtube

--- routes/v1/youtube.js
+++ routes/v1/youtube.js
@@ -11,11 +11,12 @@ router.get('/', async(req,res) => {
         for(let i = 0; i < videos.length; i++) {
             let video = {
                 title: videos[i].original_title,
-                url: `https://www.youtube.com/watch?v=${videos[i].id}`
+                url: `https://www.youtube.com/watch?v=${videos[i].id}`,
+                watched: false
             }
             playlist.push(video);
         }
-        res.status(200).json({playlist:playlist});
+        res.status(200).json({ plid: plid, playlists: playlist });
     } catch(err) {
         console.log(err.message);
         res.status(500).send(err.message);
--- scraper.js
+++ scraper.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const youtube = require('./routes/youtube');
+const youtube = require('./routes/v1/youtube');
+const YouTube = require('./routes/v2/youtube');
 
 const server = express();
 server.use(express.json());
-server.use('/youtube', youtube);
+server.use('/v1/youtube', youtube);
+server.use('/v2/youtube', YouTube);
 
 module.exports = server;
 
 if(require.main === module) {
-    const port = process.env.SCRAPER || 1680;
+    const port = process.env.SCRAPER || 1231;
     server.listen(port, () => {
         console.log(`Scraper listening on port:${port}`);
     });
